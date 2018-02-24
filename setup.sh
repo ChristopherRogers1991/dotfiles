a485be64ad81f61dc19aeae1a94da0905301615c
Chris Rogers <ChristopherRogers1991@gmail.com>
2018-02-24 10:31:14 -0500

    Fix file linking

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-cd files
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  BASE_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+BASE_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+FILES_DIR="$BASE_DIR/files"
+
+cd $FILES_DIR
 
 for f in *; do
-    ln -sb $f ~/.$f
+    ln -sb $FILES_DIR/$f ~/.$f
 done
